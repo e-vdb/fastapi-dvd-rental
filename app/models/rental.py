a9e1b0fbf2c681441b2f43bed86187d18178c50c
e.vanderbeken <e.vanderbeken@house-of-communication.com>
2025-10-10 11:03:11 +0200

    doc: add docstring

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,16 +1,19 @@
-"""
-Models for the endpoints input and responses.
-"""
+"""Models for the endpoints input and responses."""
 
-from pydantic import BaseModel
 from datetime import datetime
 
+from pydantic import BaseModel
+
+
 class RentalOutput(BaseModel):
+    """Model for the rental output."""
 
     rental_date: datetime
     title: str
 
 
 class RentalFilmCountOutput(BaseModel):
+    """Model for the rental film count output."""
+
     title: str
     count: int
