1e1bc3727c87c1a275ef58b85d18b40ff27d6dab
e.vanderbeken <e.vanderbeken@house-of-communication.com>
2025-10-10 12:29:31 +0200

    initial commit

---
+++ app/db/schemas.py
@@ -0,0 +1,55 @@
+"""Schemas for the database tables."""
+
+# pylint: disable=too-few-public-methods
+
+from sqlalchemy import DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """Base class for all schemas."""
+
+
+class Customer(Base):
+    """Schema for customer table."""
+
+    __tablename__ = "customer"
+    customer_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String)
+    last_name: Mapped[str] = mapped_column(String)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the object."""
+        return f"<Customer(first_name={self.first_name}, last_name={self.last_name})>"
+
+
+class Rental(Base):
+    """Schema for rental table."""
+
+    __tablename__ = "rental"
+    rental_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    customer_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("customer.customer_id"),
+    )
+    inventory_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("inventory.inventory_id"),
+    )
+    rental_date: Mapped[str] = mapped_column(DateTime)
+
+
+class Inventory(Base):
+    """Schema for inventory table."""
+
+    __tablename__ = "inventory"
+    inventory_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    film_id: Mapped[int] = mapped_column(Integer, ForeignKey("film.film_id"))
+
+
+class Film(Base):
+    """Schema for film table."""
+
+    __tablename__ = "film"
+    film_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String)
